f87759e5478e6eb940c96366a2b08f49aa0f6e91
sirajyamin <sirajyamin.dev@gmail.com>
2024-11-25 21:37:44 +0500

    second-commit

--- app/(main)/index.tsx
+++ app/(main)/index.tsx
@@ -1,10 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function Home() {
+const Home = () => {
   return (
     <View>
       <Text>Home</Text>
     </View>
   )
 }
+
+export default Home
--- app/(main)/settings.tsx
+++ app/(main)/settings.tsx
@@ -1,7 +1,7 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-const settings = () => {
+const Settings = () => {
   return (
     <View>
       <Text>settings</Text>
